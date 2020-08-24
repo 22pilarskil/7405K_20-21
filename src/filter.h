eeb9605c6ed69ff45e19af0ea53df336e8409cb9
JoeySorkin <joey.sorkin@outlook.com>
2020-08-23 20:35:44 -0400

    Added everything but constructor and main function to private

--- src/filter.h
+++ src/filter.h
@@ -2,8 +2,8 @@
 #include <vector>
 using namespace std;
 
-class Filter {       
-  public:             
+class Filter {
+  private:
     int mean0;
 	int var0;
 	float meanSensor;
@@ -12,9 +12,9 @@ class Filter {
 	float varMove;
 	vector<float> positions;
 	vector<float> distances;
-	Filter(int a, int b, float c, float d, float e, float f, vector<float> g, vector<float> h);
-	vector<double> get_prediction();
 	vector<double> predict(double var, double mean, double varMove, double meanMove);
-	vector<double> correct(double var, double mean, double varSensor, double meanSensor); 
-
+	vector<double> correct(double var, double mean, double varSensor, double meanSensor);
+  public:            
+    Filter(int a, int b, float c, float d, float e, float f, vector<float> g, vector<float> h);
+	vector<double> get_prediction();
 };
