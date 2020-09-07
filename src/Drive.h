5c8ac4d0503b2735b32d86a9823ffcf3b3e9a834
Zayn-Rekhi <rekhi.zayn@gmail.com>
2020-09-07 16:05:50 -0400

    Added py files

--- src/Drive.h
+++
@@ -1,7 +0,0 @@
-#include "main.h"
-
-void rollers(int coefficient, bool flip, std::string powered);
-void mecanum(int power, int strafe, int turn);
-void brake(std::string mode);
-void move_to(double new_y, double new_x, double heading, bool pure_pursuit, double scale, int coefficient, bool flip, std::string powered);
-void move_to_pure_pursuit(std::vector<std::vector<double>> points, double scale, int coefficient, bool flip, std::string powered);
