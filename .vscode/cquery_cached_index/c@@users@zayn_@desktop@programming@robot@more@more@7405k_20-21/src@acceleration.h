74ad48a780168247af6aae9d254cbf58a50f961a
Zayn-Rekhi <rekhi.zayn@gmail.com>
2020-10-15 17:13:42 -0400

    Deleted bin

--- .vscode/cquery_cached_index/c@@users@zayn_@desktop@programming@robot@more@more@7405k_20-21/src@acceleration.h
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-
-class Acceleration{
-	public:
-		 Acceleration(double _power, double _divisor);
-		 double get_curve(double controller_output);
-		 double power;
-		 double divisor;
-};
