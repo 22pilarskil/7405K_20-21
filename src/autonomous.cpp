ddc2628b62d28cfabc270d8dbc80260fa867a13b
Liam <22pilarskil@millburn.org>
2020-11-04 10:05:43 -0500

    Autonomous.cpp note

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -14,6 +14,13 @@ void autonomous()
     std::vector<std::vector<double>> points1 {{0, 0}, {50, 0.1}, {2000, -700}, {2000, 700}, {1000, 0}, {0, 0}};
     Robot::move_to_pure_pursuit(points1, {1.2, 1.2, 1.2});
 
+    /* Below is our programming skills path in its entirety. It uses the Robot::move_to, Robot::move_to_pure_pursuit, and 
+   	Robot::store functions to move to preset positions on the field and intake balls that we know to be there. It is 
+   	important to note that while in our programming skills video (https://www.youtube.com/watch?v=_H-iJ-kX9H8) it appears
+   	that our robot is detecting and chasing down balls on the field, this is not the case. Since the field is set up the 
+   	same way every time, we simply use odometry to find the points on the field where balls are located, and initiate a
+   	single, uniform sequence each run. In effect, if the field is set up incorrectly (say a ball is shifted to the left 
+   	or right by a significant margin) the odometry points will not work, and our skills path will fail. */
 
    	//Tower 1
 	Robot::reset_Balls(1, 0);
