0e1a442111f771e6bb7eb12c77fb739201af25a2
Liam <22pilarskil@millburn.org>
2021-01-09 21:21:21 -0500

    Remove random .py file

--- src/alphavantage.py
+++
@@ -1,9 +0,0 @@
-import json
-import requests
-r = requests.get(url = "https://www.alphavantage.co/query?function=SMA&symbol={}&interval={}&time_period={}&series_type={}&apikey={}".format(
-	"EYES",
-	"daily",
-	"1",
-	"1",
-	"close",
-	"MRH0YM0UZGM5K07E"))
